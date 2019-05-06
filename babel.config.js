3cb2edfcf8b1d57aa26512a431ea386c7f41a14c
zhaoweixiang <zhaoweixiang@RRCOA.COM>
2019-05-06 16:57:58 +0800

    fix: 完善babel配置

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+// {
+//   "presets": [
+//     ["@babel/stage-0"],
+//   ],
+//   "plugins": [
+//    "@babel/plugin-syntax-dynamic-import",
+//     ["@babel/plugin-proposal-decorators", { "legacy": true }],
+//     ["@babel/plugin-proposal-class-properties", { "loose": false }],
+//   ]
+// }
+module.exports = {
+  presets: [
+    // ['@babel/preset-env', {
+    //   'useBuiltIns': 'usage'
+    // }]
+    '@babel/preset-env'
+  ]
+}
--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,87 +1,3 @@
-// const webpack = require('webpack')
-// const VueLoaderPlugin = require('vue-loader/lib/plugin')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const HappyPack = require('happypack')
-// const path = require('path')
-// const { resolve } = require('path');
-// const os = require('os')
-// const happyThreadPool = HappyPack.ThreadPool({size: os.cpus().length})
-// module.exports = {
-//   entry: './src/index.js',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.vue$/,
-//         loader: 'vue-loader'
-//       },
-//       {
-//         test: /\.(sa|sc|c)ss$/,
-//         use: [
-//           'style-loader',
-//           'css-loader',
-//           'postcss-loader',
-//           'sass-loader'
-//         ]
-//       },
-//       {
-//         test: /\.less$/,
-//         use: [
-//           'style-loader',
-//           'css-loader',
-//           'postcss-loader',
-//           'less-loader'
-//         ]
-//       }, {
-//         test: /\.(png|jpg|gif)$/,
-//         use : [
-//           {
-//             loader: 'file-loader',
-//             options: {
-//               limit: 5000,
-//               name: "imgs/[name].[ext]"
-//             }
-//           }
-//         ]
-//       },
-//       {
-//         test: /\.js$/,
-//         //把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
-//         loader: 'happypack/loader?id=happyBabel',
-//         //排除node_modules 目录下的文件
-//         exclude: /node_modules/,
-//     },
-//     ]
-//   },
-//   plugins: [
-//     new webpack.HashedModuleIdsPlugin(),
-//     new VueLoaderPlugin(),
-//     new HtmlWebpackPlugin({
-//       template: path.resolve(__dirname, '../index.html')
-//     }),
-//     new HappyPack({
-//       //用id来标识 happypack处理类文件
-//       id: "happyBabel",
-//       //如何处理 用法和loader 的配置一样
-//       loaders: [
-//         {
-//           loader: "babel-loader?cacheDirectory=true"
-//         }
-//       ],
-//       //共享进程池
-//       threadPool: happyThreadPool,
-//       //允许 HappyPack 输出日志
-//       verbose: true
-//     }),
-//   ],
-//   resolve: {
-//     extensions: ['.js', '.vue'],
-//     alias: {
-//         vue: 'vue/dist/vue.esm.js',
-//         '@': resolve('src'),
-//     },
-// },
-// }
-
 const webpack = require('webpack');
 const path = require('path');
 // vue-loader 插件
@@ -93,69 +9,84 @@ const { resolve } = require('path');
 const HappyPack = require('happypack');
 const os = require('os');
 const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                //把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
-                loader: 'happypack/loader?id=happyBabel',
-                // loader: 'babel-loader',
-                //排除node_modules 目录下的文件
-                // exclude: /node_modules/,
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'url-loader',
-            },
-        ],
-    },
-    stats: { children: false },
-    plugins: [
-        // 请确保引入这个插件来施展魔法
-        new VueLoaderPlugin(),
-        // new HtmlWebpackPlugin({
-        //     template: path.resolve(__dirname, '../index.html'),
-        // }),
-        new HtmlWebpackPlugin({
-          template: './index.html'  // 模板
-       }),
-        new HappyPack({
-            //用id来标识 happypack处理类文件
-            id: 'happyBabel',
-            //如何处理 用法和loader 的配置一样
-            loaders: [
-                {
-                    loader: 'babel-loader?cacheDirectory=true',
-                },
-            ],
-            //共享进程池
-            threadPool: happyThreadPool,
-            //允许 HappyPack 输出日志
-            verbose: true,
-        }),
-        // 解决vender后面的hash每次都改变
-        new webpack.HashedModuleIdsPlugin(),
-        // new CopyWebpackPlugin([
-        //     {
-        //         from: path.resolve(__dirname, '../static'),
-        //         to: 'static',
-        //         ignore: ['.*'],
-        //     },
-        // ]),
-    ],
-    resolve: {
-        extensions: ['.js', '.vue'],
-        alias: {
-            vue: 'vue/dist/vue.esm.js',
-            '@': resolve('src'),
-        },
-    },
+  entry: {
+    main: ['@babel/polyfill', path.resolve(__dirname, '../src/index.js')]
+  },
+  module: {
+      rules: [
+          {
+              test: /\.js$/,
+              //把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
+              // loader: 'babel-loader',
+              //排除node_modules 目录下的文件
+              // loader: 'happypack/loader?id=happyBabel',
+              loader: 'babel-loader',
+              exclude: path.resolve(__dirname, "node_modules"),
+            //   use: [
+            //     {
+            //     loader:"babel-loader",
+            //     options:{
+            //         presets:[
+            //             "es2015"
+            //         ]
+            //     }
+            //   }, {
+            //     loader: 'happypack/loader?id=happyBabel',
+            //   }
+            // ]
+          },
+          {
+              test: /\.vue$/,
+              loader: 'vue-loader',
+          },
+          {
+              test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+              loader: 'url-loader',
+          },
+      ],
+  },
+  stats: { children: false },
+  plugins: [
+      // 请确保引入这个插件来施展魔法
+      new VueLoaderPlugin(),
+      // new HtmlWebpackPlugin({
+      //     template: path.resolve(__dirname, '../index.html'),
+      // }),
+      new HtmlWebpackPlugin({
+        template: './index.html'  // 模板
+      }),
+      new HappyPack({
+          //用id来标识 happypack处理类文件
+          id: 'happyBabel',
+          //如何处理 用法和loader 的配置一样
+          loaders: [
+              {
+                  loader: 'babel-loader?cacheDirectory=true',
+              },
+          ],
+          //共享进程池
+          threadPool: happyThreadPool,
+          //允许 HappyPack 输出日志
+          verbose: true,
+      }),
+      // 解决vender后面的hash每次都改变
+      new webpack.HashedModuleIdsPlugin(),
+      new CopyWebpackPlugin([
+          {
+              from: path.resolve(__dirname, '../static'),
+              to: path.resolve(__dirname, 'dist/static'),
+              ignore: ['.*'],
+          },
+      ]),
+  ],
+  resolve: {
+      extensions: ['.js', '.vue'],
+      alias: {
+          vue: 'vue/dist/vue.esm.js',
+          '@': resolve('src'),
+      },
+  }
 };
